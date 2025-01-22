3778efeee6c3f1ab7004f176715375177e4b21ff
saud-ansari <mohdsaudansari2005@gmail.com>
2025-01-22 15:44:06 +0530

    fix bug

--- src/Components/SummaryCard/Summary.js
+++ src/Components/SummaryCard/Summary.js
@@ -9,10 +9,10 @@ const Summary = () => {
             <div className='my-3'>
             <h1>Welcome, Jhonson</h1>
             <p>Thanks for coming back. Take a look at your Summary.</p>
-            <Row className='text-center bg-light rounded py-3 m-2'>
+            <Row className='text-center bg-light rounded py-3 mx-1'>
                 <Col xs={12} lg={4} className='rightBorder px-3'>
                 <Plus size={25} className='text-primary'/> Total Balance
-                    <h2 className='text-primary'>₹16,34,267.79</h2>
+                    <h2 className='text-primary'>₹6,34,267.79</h2>
                 </Col>
                 <Col xs={6} lg={4} className='rightBorder px-3 '>
                 <ArrowDown size={15} className='text-success'/>  Total Income
